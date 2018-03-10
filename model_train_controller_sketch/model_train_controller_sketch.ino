e6433e7234993977b960279094b73515d9d62758
Timothy Brook <timjbro>
2018-03-10 11:48:05 +0000

    Tested, works with Arduino Uno.
    Prototyped with three potentiometers as control inputs and an LED to verify the PWM output.

--- model_train_controller_sketch/model_train_controller_sketch.ino
+++ model_train_controller_sketch/model_train_controller_sketch.ino
@@ -1,28 +1,48 @@
 #include <MsTimer2.h>
+// MIT License
+// Copyright (c) 2018 Timothy Brook
+// For more information check out the GitHub repository:
+// https://github.com/timjbro/Model-Train-Controller-Arduino
 
+// **********************************************************************************************************
+// ***** These variables can be modified to suit the circuit, model railway scale and locomotives used. *****
+// **********************************************************************************************************
+
+// The Power, Reverser and Break lever controls are input through the ADC
 const int powerControlPin = A0;
 const int reverserControlPin = A1;
 const int breakControlPin = A2;
-const int pwmDriverPin = 13;
 
-const double adcMaxValue = 1024.0;
-const double pwmMinValue = 10.0;
-const double pwmMaxValue = 255.0;
-const double accRatio = 0.005;
-const double decRate = 0.01;
+// Timer2 is normally used for PWM on pins 3 and 11
+// As we are repurposing Timer2 as an interrupt we can't use pin 3 or 11 for the PWM drive.
+const int pwmDriverPin = 10;
 
+// Constant values for the Speed algorithm.
+const double adcMaxValue = 1024.0; // Set for Arduino Uno
+const double pwmMinValue = 10.0; // Must be just large enough to stop the DC motor from stalling.
+const double pwmMaxValue = 255.0; // Reduce this to limit the maximum speed of the locomotive.
+const double accRatio = 0.0005; // The rate of acceleration & decelleration when NOT breaking.
+const double decRate = 0.001; // This is the rate of breaking. Must be greater than the accRatio otherwise weird things happen!
+
+// **********************************************************************************************************
+// **********************************************************************************************************
+// **********************************************************************************************************
+
+// Power & Speed
 double PowerLeverPosition = 0;
 double TargetSpeed = 0;
-double ActualSpeed = 0;
+volatile double ActualSpeed = 0;
 
+// Reverser & Direction
 enum ReverserStates { Reverse, Neutral, Forward };
 ReverserStates ReverserLeverPosition = Neutral;
 ReverserStates ActualDirection = Neutral;
 
+// Break
 enum BreakStates { Release, On1, On2, FullService, Emergency };
 BreakStates BreakLeverPosition = FullService;
 
-void dspRecalculate ()
+void dspRecalculateInterrupt ()
 {
   if (ActualSpeed < pwmMinValue)
   {
@@ -72,15 +92,14 @@ void setup() {
   pinMode(reverserControlPin, INPUT);
   pinMode(breakControlPin, INPUT);
   pinMode(pwmDriverPin, OUTPUT);
-  analogWrite(pwmDriverPin, 0);
-  MsTimer2::set(100, dspRecalculate); // 100ms period.
+  MsTimer2::set(10, dspRecalculateInterrupt); // 10ms ISR period.
   MsTimer2::start();
 }
 
 void loop() {
   noInterrupts();
   // Power
-  PowerLeverPosition = ((double)analogRead(powerControlPin) / adcMaxValue) * pwmMaxValue;
+  PowerLeverPosition = (double)analogRead(powerControlPin);
   // Reverser
   int reverserValue = analogRead(reverserControlPin);
   if (reverserValue < 200)
@@ -90,7 +109,7 @@ void loop() {
   else
     ReverserLeverPosition = Neutral;
   // Break
-  int breakValue = analogRead(reverserControlPin);
+  int breakValue = analogRead(breakControlPin);
   if (breakValue > 800)
     BreakLeverPosition = Emergency;
   else if (breakValue > 600)
@@ -105,5 +124,5 @@ void loop() {
   SetDirection();
   SetTargetSpeed();
   interrupts();
-  delay(500);
+  delay(100); // This delay is required as it allows time for the ISR to run.
 }
